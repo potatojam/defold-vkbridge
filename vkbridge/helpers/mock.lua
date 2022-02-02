dff7a0cda2c75210c7c433017fedcab13723f16b
Bagabum <Bagabum1@gmail.com>
2022-02-02 15:09:44 +0300

    Fixed events and mock

--- vkbridge/helpers/mock.lua
+++ vkbridge/helpers/mock.lua
@@ -1,15 +1,16 @@
 local rxi_json = require("vkbridge.helpers.json")
+local helper = require("vkbridge.helpers.helper")
+local events = require("vkbridge.events")
 
 local M = {listeners = {}}
 
-local GLOBAL_CALLBACK_ID = 0
-local NO_ERR = nil
+local storage = {}
 
 --
 --
 --
 
-function M.send(cb_id, message_id, message)
+function M.vk_send(cb_id, message_id, message)
     timer.delay(0.001, false, function(self)
         local count = #M.listeners
         for i = count, 1, -1 do
@@ -36,338 +37,70 @@ function M.remove_listener(listener)
     end
 end
 
+function M.send(cb_id, message_id, message)
+    if message_id == events.SHOW_NATIVE_ADS or message_id == events.CHECK_NATIVE_ADS then
+        M.vk_send(cb_id, nil, rxi_json.encode({result = true}))
+    elseif message_id == events.STORAGE_SET then
+        local data = rxi_json.decode(message)
+        storage[data.key] = data.value
+        M.vk_send(cb_id, nil, rxi_json.encode({result = true}))
+    elseif message_id == events.STORAGE_GET then
+        local data = rxi_json.decode(message)
+        local ex_data = {}
+        for i, key in ipairs(data.keys) do
+            local obj = {}
+            obj.key = key
+            obj.value = storage[key] or ""
+            table.insert(ex_data, obj)
+        end
+        M.vk_send(cb_id, nil, rxi_json.encode({keys = ex_data}))
+    elseif message_id == events.STORAGE_GET_KEYS then
+        local data = rxi_json.decode(message)
+        local offset = data.offset or 0
+        local count = data.count or 0
+        local ex_data = {}
+        local i = 0
+        for key, value in pairs(storage) do
+            i = i + 1
+            if i > offset + count then
+                break
+            end
+            if i > offset then
+                table.insert(ex_data, key)
+            end
+        end
+        M.vk_send(cb_id, nil, rxi_json.encode({keys = ex_data}))
+    else
+        M.vk_send(cb_id, nil, message)
+    end
+end
+
 function M.init_callbacks()
-
 end
 
---
--- Yandex Games SDK
---
-
-function M.show_fullscreen_adv(cb_id)
-    M.send(cb_id, "close", false)
+function M.supports(name)
+    for key, e_name in pairs(events) do
+        if name == e_name then
+            return true
+        end
+    end
+    return false
 end
 
-function M.show_rewarded_video(cb_id)
-    M.send(cb_id, "close")
+function M.is_webview()
+    return false
 end
 
-function M.open_auth_dialog(cb_id)
-    M._auth = true
-    M.send(cb_id, NO_ERR)
-end
-
-function M.clipboard_write_text(cb_id)
-    M.send(cb_id, "Not supported.")
-end
-
-function M.device_info_type()
-    return "desktop"
-end
-
-function M.device_info_is_desktop()
+function M.is_standalone()
     return true
 end
 
-function M.device_info_is_mobile()
+function M.is_iframe()
     return false
 end
 
-function M.device_info_is_tablet()
+function M.is_embedded()
     return false
-end
-
-function M.environment()
-    return "{\"app\":{\"id\":\"1\"},\"payload\":\"test\",\"i18n\":{\"tld\":\"en\",\"lang\":\"en\"},\"browser\":{\"lang\":\"en\"}}"
-end
-
-function M.feedback_can_review(cb_id)
-    M.send(cb_id, NO_ERR, rxi_json.encode({value = false, reason = "UNKNOWN"}))
-end
-
-function M.feedback_request_review(cb_id)
-    M.send(cb_id, NO_ERR, rxi_json.encode({feedbackSent = false}))
-end
-
--- TODO:
--- "get_leaderboards"
--- "leaderboards_get_description"
--- "leaderboards_get_player_entry"
--- "leaderboards_set_score"
--- "leaderboards_get_entries"
-function M.get_leaderboards(cb_id)
-    M.send(cb_id, "Leaderboards is not available yet.")
-end
-
-function M.get_payments(cb_id, options)
-    assert(type(options) == "string")
-
-    options = rxi_json.decode(options)
-    if not M._player then
-        M.send(cb_id, "FetchError: Unauthorized")
-    else
-        M._payments = {
-            catalog = {
-                {
-                    ["id"] = "item1_example",
-                    ["title"] = "1000 монет",
-                    ["description"] = "Отключение рекламы",
-                    ["imageURI"] = "https://avatars.mds.yandex.net/get-bunker/50064/9c8dea8c94b9bea5ef1677932c8272cbfca4995a/orig",
-                    ["price"] = "49 ₽",
-                    ["priceValue"] = "49",
-                    ["priceCurrencyCode"] = "RUR"
-                }
-            },
-            purchases = {},
-            signed = options.signed
-        }
-
-        M.send(cb_id, NO_ERR)
-    end
-end
-
-function M.payments_purchase(cb_id, options)
-    assert(M._payments)
-    assert(type(options) == "string")
-
-    options = rxi_json.decode(options)
-    assert(type(options.id) == "string")
-
-    local result = {productID = options.id, purchaseToken = "token_example"}
-
-    if options.developerPayload then
-        result.developerPayload = options.developerPayload
-    end
-
-    if M._payments.signed then
-        result.signature = "signature_example"
-    end
-
-    table.insert(M._payments.purchases, result)
-
-    M.send(cb_id, NO_ERR, rxi_json.encode(result))
-end
-
-function M.payments_get_purchases(cb_id)
-    assert(M._payments)
-
-    local tmp = {purchases = M._payments.purchases}
-    if M._payments.signed then
-        tmp.signature = "signature_example"
-    end
-
-    M.send(cb_id, NO_ERR, rxi_json.encode(tmp))
-end
-
-function M.payments_get_catalog(cb_id)
-    assert(M._payments)
-
-    M.send(cb_id, NO_ERR, rxi_json.encode(M._payments.catalog))
-end
-
-function M.payments_consume_purchase(cb_id, purchase_token)
-    assert(M._payments)
-    assert(type(purchase_token) == "string")
-
-    local to_remove = nil
-    for i, v in pairs(M._payments.purchases) do
-        if v.purchaseToken == purchase_token then
-            to_remove = i
-            break
-        end
-    end
-
-    if to_remove == nil then
-        M.send(cb_id, "FetchError: Not Found")
-    else
-        table.remove(M._payments.purchases, to_remove)
-        M.send(cb_id, NO_ERR)
-    end
-end
-
-function M.get_player(cb_id, options)
-    assert(type(options) == "string")
-    if not M._auth then
-        M.send(cb_id, "FetchError: Unauthorized")
-    else
-        M._player = {
-            name = "Mock",
-            photo = {
-                small = "https://games.yandex.ru/api/sdk/v1/player/avatar/0/islands-retina-small",
-                medium = "https://games.yandex.ru/api/sdk/v1/player/avatar/0/islands-retina-middle",
-                large = "https://games.yandex.ru/api/sdk/v1/player/avatar/0/islands-200"
-            },
-            unique_id = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-            data = {},
-            stats = {}
-        }
-
-        M.send(cb_id, NO_ERR)
-    end
-end
-
-function M.player_get_id()
-    assert(M._player)
-
-    return M._player.unique_id
-end
-
-function M.player_get_ids_per_game(cb_id)
-    assert(M._player)
-
-    M.send(cb_id, NO_ERR, "[{\"appID\":100,\"userID\":\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\"}]")
-end
-
-function M.player_get_name()
-    assert(M._player)
-
-    return M._player.name
-end
-
-function M.player_get_photo(size)
-    assert(M._player)
-    assert(type(size) == "string")
-
-    return M._player.photo[size]
-end
-
-function M.player_get_unique_id()
-    assert(M._player)
-
-    return M._player.unique_id
-end
-
-function M.player_set_data(cb_id, data, flush)
-    assert(M._player)
-    assert(type(data) == "string")
-
-    M._player.data = rxi_json.decode(data)
-    M.send(cb_id, NO_ERR)
-end
-
-function M.player_get_data(cb_id, keys)
-    assert(M._player)
-    assert(type(keys) == "string" or type(keys) == "nil")
-
-    if keys then
-        keys = rxi_json.decode(keys)
-        local tmp = {}
-        for _, key in pairs(keys) do
-            tmp = M._player.data[key]
-        end
-        M.send(cb_id, NO_ERR, rxi_json.encode(tmp))
-    else
-        M.send(cb_id, NO_ERR, rxi_json.encode(M._player.data))
-    end
-end
-
-function M.player_set_stats(cb_id, stats)
-    assert(M._player)
-    assert(type(stats) == "string")
-
-    M._player.stats = rxi_json.decode(stats)
-    M.send(cb_id, NO_ERR)
-end
-
--- Result: {"stats":{"v1":99,"v2":13.333,"v3":2},"newKeys":["v3"]} 
-function M.player_increment_stats(cb_id, increments)
-    assert(M._player)
-    assert(type(increments) == "string")
-
-    increments = rxi_json.decode(increments)
-
-    local new_keys = {}
-    for k, v in pairs(increments) do
-        if M._player.stats[k] ~= nil then
-            M._player.stats[k] = M._player.stats[k] + v
-        else
-            M._player.stats[k] = v
-            table.insert(new_keys, k)
-        end
-    end
-
-    M.send(cb_id, NO_ERR, rxi_json.encode({stats = M._player.stats, newKeys = new_keys}))
-end
-
-function M.player_get_stats(cb_id, keys)
-    assert(M._player)
-    assert(type(keys) == "string" or type(keys) == "nil")
-
-    if keys then
-        keys = rxi_json.decode(keys)
-        local tmp = {}
-        for _, key in pairs(keys) do
-            tmp = M._player.stats[key]
-        end
-        M.send(cb_id, NO_ERR, rxi_json.encode(tmp))
-    else
-        M.send(cb_id, NO_ERR, rxi_json.encode(M._player.stats))
-    end
-end
-
-function M.screen_fullscreen_status()
-    return "off"
-end
-
-function M.screen_fullscreen_request(cb_id)
-    M.send(cb_id, "Not supported.")
-end
-
-function M.screen_fullscreen_exit(cb_id)
-    M.send(cb_id, "Not supported.")
-end
-
-function M.get_storage(cb_id)
-    M._storage = {}
-    M.send(cb_id, NO_ERR)
-end
-
-function M.storage_get_item(key, value)
-    return M._storage[key]
-end
-
-function M.storage_set_item(key, value)
-    M._storage[key] = value
-end
-
-function M.storage_remove_item(key)
-    M._storage[key] = nil
-end
-
-function M.storage_clear()
-    M._storage = {}
-end
-
-function M.storage_key(n)
-    local keys = {}
-    for k, _ in pairs(M._storage) do
-        table.insert(keys, k)
-    end
-    return keys[n + 1]
-end
-
-function M.storage_length()
-    local c = 0
-    for _ in pairs(M._storage) do
-        c = c + 1
-    end
-    return c
-end
-
-function M.banner_init(cb_id)
-    M.send(cb_id, "Error loading SDK.")
-end
-
-function M.banner_create(rtb_id, options, cb_id)
-end
-
-function M.banner_refresh(rtb_id, cb_id)
-end
-
-function M.banner_destroy(rtb_id)
-end
-
-function M.banner_set(rtb_id, property, value)
 end
 
 return {
@@ -375,7 +108,7 @@ return {
         if not vkbridge_private then
             vkbridge_private = M
 
-            M.send(GLOBAL_CALLBACK_ID, "init")
+            M.vk_send(helper.VKBRIDGE_INIT_ID, "init")
         end
     end
 }
--- vkbridge/vkbridge.lua
+++ vkbridge/vkbridge.lua
@@ -2,68 +2,13 @@ local rxi_json = require("vkbridge.helpers.json")
 local mock = require("vkbridge.helpers.mock")
 local helper = require("vkbridge.helpers.helper")
 local listeners = require("vkbridge.helpers.listeners")
+local events = require("vkbridge.events")
 
 --
 -- HELPERS
 --
 
 local M = {vkbridge_ready = false}
-
-M.INIT = "VKWebAppInit"
-M.GET_COMMUNITY_AUTH_TOKEN = "VKWebAppGetCommunityAuthToken"
-M.ADD_TO_COMMUNITY = "VKWebAppAddToCommunity"
-M.ADD_TO_HOME_SCREEN_INFO = "VKWebAppAddToHomeScreenInfo"
-M.CLOSE = "VKWebAppClose"
-M.COPY_TEXT = "VKWebAppCopyText"
-M.CREATE_HASH = "VKWebAppCreateHash"
-M.GET_USER_INFO = "VKWebAppGetUserInfo"
-M.SET_LOCATION = "VKWebAppSetLocation"
-M.SEND_TO_CLIENT = "VKWebAppSendToClient"
-M.GET_CLIENT_VERSION = "VKWebAppGetClientVersion"
-M.GET_PHONE_NUMBER = "VKWebAppGetPhoneNumber"
-M.GET_EMAIL = "VKWebAppGetEmail"
-M.GET_GROUP_INFO = "VKWebAppGetGroupInfo"
-M.GET_GEODATA = "VKWebAppGetGeodata"
-M.GET_COMMUNITY_TOKEN = "VKWebAppGetCommunityToken"
-M.GET_CONFIG = "VKWebAppGetConfig"
-M.GET_LAUNCH_PARAMS = "VKWebAppGetLaunchParams"
-M.SET_TITLE = "VKWebAppSetTitle"
-M.GET_AUTH_TOKEN = "VKWebAppGetAuthToken"
-M.CALL_API_METHOD = "VKWebAppCallAPIMethod"
-M.JOIN_GROUP = "VKWebAppJoinGroup"
-M.LEAVE_GROUP = "VKWebAppLeaveGroup"
-M.ALLOW_MESSAGES_FROM_GROUP = "VKWebAppAllowMessagesFromGroup"
-M.DENY_NOTIFICATIONS = "VKWebAppDenyNotifications"
-M.ALLOW_NOTIFICATIONS = "VKWebAppAllowNotifications"
-M.OPEN_PAY_FORM = "VKWebAppOpenPayForm"
-M.OPEN_APP = "VKWebAppOpenApp"
-M.SHARE = "VKWebAppShare"
-M.SHOW_WALL_POST_BOX = "VKWebAppShowWallPostBox"
-M.SCROLL = "VKWebAppScroll"
-M.SHOW_ORDER_BOX = "VKWebAppShowOrderBox"
-M.SHOW_LEADER_BOARD_BOX = "VKWebAppShowLeaderBoardBox"
-M.SHOW_INVITE_BOX = "VKWebAppShowInviteBox"
-M.SHOW_REQUEST_BOX = "VKWebAppShowRequestBox"
-M.ADD_TO_FAVORITES = "VKWebAppAddToFavorites"
-M.SHOW_COMMUNITY_WIDGET_PREVIEW_BOX = "VKWebAppShowCommunityWidgetPreviewBox"
-M.SHOW_STORY_BOX = "VKWebAppShowStoryBox"
-M.STORAGE_GET = "VKWebAppStorageGet"
-M.STORAGE_GET_KEYS = "VKWebAppStorageGetKeys"
-M.STORAGE_SET = "VKWebAppStorageSet"
-M.FLASH_GET_INFO = "VKWebAppFlashGetInfo"
-M.SUBSCRIBE_STORY_APP = "VKWebAppSubscribeStoryApp"
-M.OPEN_WALL_POST = "VKWebAppOpenWallPost"
-M.CHECK_ALLOWED_SCOPES = "VKWebAppCheckAllowedScopes"
-M.CHECK_NATIVE_ADS = "VKWebAppCheckNativeAds"
-M.SHOW_NATIVE_ADS = "VKWebAppShowNativeAds"
-M.RETARGETING_PIXEL = "VKWebAppRetargetingPixel"
-M.CONVERSION_HIT = "VKWebAppConversionHit"
-M.RESUZE_WINDOW = "VKWebAppResizeWindow"
-M.ADD_TO_MENU = "VKWebAppAddToMenu"
-M.SHOW_SUBSCRIPTION_BOX = "VKWebAppShowSubscriptionBox"
-M.SHOW_INSTALL_PUSH_BOX = "VKWebAppShowInstallPushBox"
-M.GET_FRIENDS = "VKWebAppGetFriends"
-M.SHOW_IMAGES = "VKWebAppShowImages"
 
 local init_callback = nil
 
@@ -160,10 +105,11 @@ end
 
 ---Checks if an event is available on the current device
 ---@param name string The VK Bridge method
+---@return boolean
 function M.supports(name)
     assert(type(name) == "string", "The VK Bridge method must be set.")
 
-    vkbridge_private.supports(name)
+    return vkbridge_private.supports(name)
 end
 
 ---Returns `true` if VK Bridge is running in mobile app, or `false` if not
@@ -193,27 +139,27 @@ end
 ---Check if there is the interstitial ad available to serve
 ---@param callback function callback with response data `function(self, err, data)`. If successful: `err = nil`.
 function M.check_interstitial(callback)
-    M.send(M.CHECK_NATIVE_ADS, {ad_format = "interstitial"}, callback)
+    M.send(events.CHECK_NATIVE_ADS, {ad_format = "interstitial"}, callback)
 end
 
 ---Show interstitial ads
 ---@param callback function callback with response data `function(self, err, data)`. If successful: `err = nil`.
 function M.show_interstitial(callback)
-    M.send(M.SHOW_NATIVE_ADS, {ad_format = "interstitial"}, callback)
+    M.send(events.SHOW_NATIVE_ADS, {ad_format = "interstitial"}, callback)
 end
 
 ---Check if there is the rewarded ad available to serve
 ---@param use_waterfall boolean|nil Whether to use the mechanism for displaying interstitial advertising in the absence of rewarded video.
 ---@param callback function callback with response data `function(self, err, data)`. If successful: `err = nil`.
 function M.check_rewarded(use_waterfall, callback)
-    M.send(M.CHECK_NATIVE_ADS, {ad_format = "interstitial", use_waterfall = use_waterfall}, callback)
+    M.send(events.CHECK_NATIVE_ADS, {ad_format = "interstitial", use_waterfall = use_waterfall}, callback)
 end
 
 ---Show rewarded ads
 ---@param use_waterfall boolean|nil Whether to use the mechanism for displaying interstitial advertising in the absence of rewarded video.
 ---@param callback function callback with response data `function(self, err, data)`. If successful: `err = nil`.
 function M.show_rewarded(use_waterfall, callback)
-    M.send(M.SHOW_NATIVE_ADS, {ad_format = "reward", use_waterfall = use_waterfall}, callback)
+    M.send(events.SHOW_NATIVE_ADS, {ad_format = "reward", use_waterfall = use_waterfall}, callback)
 end
 
 ---Set the value of the variable whose name is passed in the `key` parameter. `Key` life is 1 year.
@@ -223,7 +169,7 @@ end
 function M.storage_set(key, value, callback)
     assert(type(key) == "string", "Wrong key format. Must be string.")
     assert(type(value) == "string", "Wrong value format. Must be string.")
-    M.send(M.STORAGE_SET, {key = key, value = value}, callback)
+    M.send(events.STORAGE_SET, {key = key, value = value}, callback)
 end
 
 ---Return the values of the variables.
@@ -235,7 +181,7 @@ function M.storage_get(keys, callback)
     if type(keys) == "string" then
         keys = {keys}
     end
-    M.send(M.STORAGE_GET, {keys = keys}, callback)
+    M.send(events.STORAGE_GET, {keys = keys}, callback)
 end
 
 ---Return the names of all variables.
@@ -244,7 +190,7 @@ end
 ---@param callback function callback with response data `function(self, err, data)`. If successful: `err = nil`.
 function M.storage_get_keys(count, offset, callback)
     assert(type(count) == "number", "Wrong count format. Must be number.")
-    M.send(M.STORAGE_GET_KEYS, {count = count, offset = offset}, callback)
+    M.send(events.STORAGE_GET_KEYS, {count = count, offset = offset}, callback)
 end
 
 return M
